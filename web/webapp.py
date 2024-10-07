14418f1d42602a0f4848cda15b78a8c5b492975b
Dustin Lang <dstndstn@gmail.com>
2024-10-07 20:26:33 +0000

    add page drilling down on L1 events

--- web/webapp.py
+++ web/webapp.py
@@ -5,26 +5,37 @@ import sys
 
 from flask_sqlalchemy import SQLAlchemy
 
-from chord_frb_db.models import Event
+from chord_frb_db.models import Event, EventBeam
 
 import sqlalchemy as sa
 
 #from flask import session
-
-print('Config:', Config)
+#print('Config:', Config)
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 
+@app.route('/l1-events/<int:event_id>')
+def l1_event_list(event_id):
+    query = sa.select(EventBeam).filter_by(event_id=event_id)
+    r = db.session.execute(query).scalars()
+    print('r:', r)
+
+    query = sa.select(Event).filter_by(event_id=event_id)
+    event = db.session.execute(query).scalar_one()
+    print('event:', event)
+
+    fields = ['beam', 'snr', 'timestamp_utc', 'timestamp_fpga']
+    return render_template('l1_event_list.html', event_id=event_id,
+                           event=event, l1_events=r, fields=fields)
+
 @app.route("/")
-#def hello_world():
-#    return "<p>Hello, World!</p>"
-def hello(): #(name=None):
+def event_list(): #(name=None):
     query = sa.select(Event).order_by(Event.event_id)#.desc())
     #order_by(Event.timestamp.desc())
     print('Query:', query)
-    print(dir(query))
+    #print(dir(query))
 
     # #print('Count:', query.count())
     # #count_query = query.statement.with_only_columns([sa.func.count()]).order_by(None)
@@ -36,7 +47,7 @@ def hello(): #(name=None):
     # n_events = r
 
     page = request.args.get("page")
-    print('page:', page)
+    #print('page:', page)
     try:
         page = int(page)
     except:
@@ -45,11 +56,9 @@ def hello(): #(name=None):
     event_pager = db.paginate(query, page=page, per_page=20, error_out=False)
     events = event_pager.items
     
-    #print('Events is', type(events), events)
-
     fields = [ 'event_id', 'timestamp', 'rfi_grade', 'total_snr', 'dm', 'ra', 'dec', 'nbeams', 'dm_ne2001', 'dm_ymw2016', 'flux', 'fluence', 'pulse_width' ]
 
-    return render_template('hello.html', event_pager=event_pager, events=events, fields=fields)
+    return render_template('event_list.html', event_pager=event_pager, events=events, fields=fields)
 
 
 
