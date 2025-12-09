b3261cc05a2c5cffa6a79448b408d5d4290c5652
Dustin Lang <dstndstn@gmail.com>
2025-12-09 20:43:52 +0000

    itrs to RA,Dec

--- chord_frb_sifter/chord.py
+++ chord_frb_sifter/chord.py
@@ -1,4 +1,7 @@
 import numpy as np
+from astropy.coordinates import SkyCoord, EarthLocation
+from astropy.time import Time
+import astropy.units as u
 
 class Telescope(object):
     pass
@@ -24,6 +27,8 @@ class Chord(Telescope):
         self.origin_itrs_lat_deg = conf.get('origin_itrs_lat_deg', 0.0)
         #self.dish_coelev_deg = conf.get('dish_coelev_deg', 0.0)
         print('Config file: lat/long', self.origin_itrs_lat_deg, self.origin_itrs_lon_deg, 'deg')
+        self.location = EarthLocation(lat=self.origin_itrs_lat_deg * u.deg,
+                                      lon=self.origin_itrs_lon_deg * u.deg)
 
         sin_lon = np.sin(np.deg2rad(self.origin_itrs_lon_deg))
         cos_lon = np.cos(np.deg2rad(self.origin_itrs_lon_deg))
@@ -39,7 +44,7 @@ class Chord(Telescope):
             [ cos_lat*cos_lon,  cos_lat*sin_lon, sin_lat]
         ])
 
-    def grid_to_topocentric(self, x, y):
+    def grid_to_topocentric(self, x, y, z=None):
         '''
         "grid" coordinates: aligned with the dish spacings;
         X is E/W separation,
@@ -50,11 +55,15 @@ class Chord(Telescope):
         X is East
         Y is North
         '''
-        #scalar = np.isscalar(x + y)
         # Transpose for inverse
-        ze = np.zeros_like(x+y)
-        xyz = np.stack((x+ze, y+ze, ze))
-        print('xyz shape', xyz.shape)
+        if z is None:
+            # get z the right shape for broadcasting
+            # assume z = 0...
+            z = np.zeros_like(x+y)
+            x = x + z
+            y = y + z
+        xyz = np.stack((x, y, z))
+        #print('xyz shape', xyz.shape)
         topo = np.matmul(self.R_topo_to_grid.T, xyz)
         return topo
 
@@ -62,6 +71,23 @@ class Chord(Telescope):
         # Inverse transform, use R transpose.
         v_itrs = np.matmul(self.R_itrs_to_topo.T, vec)
         return v_itrs
+
+    def itrs_to_radec(self, itrs, time):
+        '''
+        "time": astropy Time object
+
+        Returns (ra, dec) in degrees.
+        '''
+        sc = SkyCoord(x=itrs[0], y=itrs[1], z=itrs[2], representation_type='cartesian',
+                      obstime=time, frame='itrs', location=self.location)
+        icrs = sc.transform_to('icrs')
+        return (icrs.ra.deg, icrs.dec.deg)
+
+    def grid_to_radec(self, x, y, z, time):
+        topo = self.grid_to_topocentric(x, y, z)
+        itrs = self.tocentric_to_itrs(topo)
+        rd = self.itrs_to_radec(itrs, time)
+        return rd
 
 if __name__ == '__main__':
     import yaml
@@ -83,10 +109,131 @@ if __name__ == '__main__':
                 (np.array([0., 1., 2.]), 0),
                 (np.array([0., 1., 2.]), np.array([3,4,5])),
                 ]:
+        print()
         topo = chord.grid_to_topocentric(x, y)
         print('x,y', x,y, '-> topo:')
         print(topo)
         print('-> ITRS:')
         itrs = chord.topocentric_to_itrs(topo)
         print(itrs)
+
+        t0 = Time('2026-01-01T00:00:00Z', scale='utc')
+        #dt = np.linspace(0, 365.2425, 100) * u.day
+        #t = t0 + dt
+        t = t0
+        print('x', itrs[0])
+        print('y', itrs[1])
+        print('z', itrs[2])
+        sc = SkyCoord(x=itrs[0], y=itrs[1], z=itrs[2], representation_type='cartesian',
+                      obstime=t, frame='itrs')
+
+        print('sky coord:', sc)
+        cirs = sc.transform_to('cirs')
+        print('cirs:', cirs)
+        
+
+
+    x,y = 1.,0.
+    topo = chord.grid_to_topocentric(x, y)
+    itrs_x = chord.topocentric_to_itrs(topo)
+    x,y = 0.,1.
+    topo = chord.grid_to_topocentric(x, y)
+    itrs_y = chord.topocentric_to_itrs(topo)
+
+    itrs_z = np.cross(itrs_x, itrs_y)
+    itrs = itrs_z
+
+    # Look a bit north -- so there's a + projection onto the y axis.
+    y = np.sin(np.deg2rad(5.))
+    x = 0.
+    z = np.sqrt(1. - x**2 - y**2)
+    topo = chord.grid_to_topocentric(x, y, z=z)
+    itrs2 = chord.topocentric_to_itrs(topo)
+
+    # Look a bit east -- so there's a + projection onto the x axis.
+    y = 0.
+    x = np.sin(np.deg2rad(5.))
+    z = np.sqrt(1. - x**2 - y**2)
+    topo = chord.grid_to_topocentric(x, y, z=z)
+    itrs3 = chord.topocentric_to_itrs(topo)
+
+    location = EarthLocation(lat=chord.origin_itrs_lat_deg, lon=chord.origin_itrs_lon_deg)
+    #print('Location', location)
+    #location=None
+
+    t = t0
+    sc = SkyCoord(x=itrs[0], y=itrs[1], z=itrs[2], representation_type='cartesian',
+                  obstime=t, frame='itrs', location=location)
+    print('sky coord:', sc)
+    cirs = sc.transform_to('cirs')
+    print('cirs:', cirs)
+    icrs = sc.transform_to('icrs')
+    print('icrs:', icrs)
+
+    from astropy.coordinates import AltAz
+    
+    ra,dec = [],[]
+    datetm = []
+
+    ra_aa,dec_aa = [],[]
+
+    ra2,dec2 = [],[]
+    ra3,dec3 = [],[]
+
+    ra4,dec4 = [],[]
+
+    t0 = Time('2026-01-01T00:00:00Z', scale='utc')
+    #dt = np.linspace(0, 365.2425, 100) * u.day
+    dt = np.linspace(0, 1, 100)
+    for dti in dt:
+        t = t0 + dti * u.day
+        sc = SkyCoord(x=itrs[0], y=itrs[1], z=itrs[2], representation_type='cartesian',
+                      obstime=t, frame='itrs', location=location)
+        icrs = sc.transform_to('icrs')
+        ra.append(icrs.ra.deg)
+        dec.append(icrs.dec.deg)
+        datetm.append(t.to_datetime())
+
+        sc = SkyCoord(x=itrs2[0], y=itrs2[1], z=itrs2[2], representation_type='cartesian',
+                      obstime=t, frame='itrs', location=location)
+        icrs = sc.transform_to('icrs')
+        ra2.append(icrs.ra.deg)
+        dec2.append(icrs.dec.deg)
+
+        sc = SkyCoord(x=itrs3[0], y=itrs3[1], z=itrs3[2], representation_type='cartesian',
+                      obstime=t, frame='itrs', location=location)
+        icrs = sc.transform_to('icrs')
+        ra3.append(icrs.ra.deg)
+        dec3.append(icrs.dec.deg)
+
+        r,d = chord.itrs_to_radec(itrs3, t)
+        ra4.append(r)
+        dec4.append(d)
+
+        sc = SkyCoord(alt=90. * u.deg, az=0. * u.deg, frame='altaz',
+                      obstime=t, location=location)
+        icrs = sc.transform_to('icrs')
+        ra_aa.append(icrs.ra.deg)
+        dec_aa.append(icrs.dec.deg)
+
+    import matplotlib
+    matplotlib.use('Agg')
+    import pylab as plt
+    
+    plt.clf()
+    #plt.plot(datetm, ra, '-', label='RA (deg)')
+    #plt.plot(datetm, dec, '-', label='Dec (deg)')
+    plt.plot(dt, ra, '-', label='zhat RA (deg)')
+    plt.plot(dt, dec, '-', label='zhat Dec (deg)')
+    plt.plot(dt, ra2, '-', label='North RA (deg)')
+    plt.plot(dt, dec2, '-', label='North Dec (deg)')
+    plt.plot(dt, ra3, '-', label='East RA (deg)')
+    plt.plot(dt, dec3, '-', label='East Dec (deg)')
+    plt.plot(dt, ra4, '--', label='East RA (deg)')
+    plt.plot(dt, dec4, '--', label='East Dec (deg)')
+    plt.plot(dt, ra_aa, '--', label='Up RA (deg)')
+    plt.plot(dt, dec_aa, '--', label='Up Dec (deg)')
+    plt.legend()
+    plt.xlabel('Time')
+    plt.savefig('rd.png')
     
